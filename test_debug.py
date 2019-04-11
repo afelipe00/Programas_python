059e8b8e4fded503a1fa55df331d7e72eb487e24
Felipe Díaz <afelipe00@users.noreply.github.com>
2019-04-10 21:16:10 -0500

    prueba de debug y commit 1

--- test_debug.py
+++ test_debug.py
@@ -2,14 +2,15 @@ a=2
 b=3
 c = a + b
 d = 0
+c = 7
 for i in range(5):
     d = d + c
     print("paso ",i," : ",d)
 print("este es el valor final: ",d)
+print("este es el valor : ",d)
 print("este es el valor final: ",d)
-print("este es el valor final: ",d)
-print("este es el valor final: ",d)
-for i in range(5):
+print("este es el valor: ",d)
+for i in range(7):
     d = d + c + c
     print("paso ",i," : ",d)
 print("este es el valor final: ",d)
