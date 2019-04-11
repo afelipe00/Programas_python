61582fa8bb6236699f2a7cb5321f9b353e0e53a0
Felipe Díaz <afelipe00@users.noreply.github.com>
2019-04-10 21:03:19 -0500

    prueba para debug

---
+++ test_debug.py
@@ -0,0 +1,15 @@
+a=2
+b=3
+c = a + b
+d = 0
+for i in range(5):
+    d = d + c
+    print("paso ",i," : ",d)
+print("este es el valor final: ",d)
+print("este es el valor final: ",d)
+print("este es el valor final: ",d)
+print("este es el valor final: ",d)
+for i in range(5):
+    d = d + c + c
+    print("paso ",i," : ",d)
+print("este es el valor final: ",d)
